26d52c5ac57927a1077cfdea0d56affbd38fb6d4
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-10-09 14:53:32 -0600

    finished lab

--- server/models.py
+++ server/models.py
@@ -14,6 +14,21 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('You must provide a name')
+        elif name in db.session.query(Author.name).all():
+            raise ValueError('You must provide a unique name')
+        
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError('Phone number must be 10 characters long')
+        
+        return phone_number
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -30,3 +45,33 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        CLICKBAIT = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not title:
+            raise ValueError('You must provide a title')
+        elif title not in CLICKBAIT:
+            raise ValueError(f'The title must include one or more of the following: {CLICKBAIT}')
+        
+        return title
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError('Summary must be less than 250 characters long')
+        
+        return summary
+    @validates('category')
+    def validate_category(self, key, category):
+        CATEGORIES = ['Fiction', 'Non-Fiction']
+        if category not in CATEGORIES:
+            raise ValueError(f'Category must be one of the following: {CATEGORIES}')
+        
+        return category
+    
